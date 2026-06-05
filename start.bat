df3e77f72c7966d32591a19c6145707abe3f28d6
Nader Zamrawi <hermannnslol@gmail.com>
2026-06-05 12:25:32 +0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting SynthAnalyst...
+
+:: Start FastAPI in a new window
+start "FastAPI" cmd /k "cd /d %~dp0 && python main.py"
+
+:: Start ngrok in a new window
+start "ngrok" cmd /k "cd /d %~dp0 && ngrok http 5678"
+
+echo Done! Both services are running.
+echo FastAPI: http://localhost:8000
+echo ngrok dashboard: http://127.0.0.1:4040
